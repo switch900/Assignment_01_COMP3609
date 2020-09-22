511870836fcb447ecf538d7c24abad3b282dcff3
Andrew <35283726+switch900@users.noreply.github.com>
2020-09-22 11:35:49 -0700

    Add project files.

---
+++ Assignment_01_COMP3609_Andrew_Hewitson/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assignment_01_COMP3609_Andrew_Hewitson
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
